78a43d97bd4b728b3c370c04f5dd71156c2165d5
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-04 23:24:11 +0530

    feat: load live matches in live page's ngOnInit() method

--- src/app/pages/live/live.component.ts
+++ src/app/pages/live/live.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../../services/api-call.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-live',
   imports: [],
   templateUrl: './live.component.html',
-  styleUrl: './live.component.css'
+  styleUrl: './live.component.css',
 })
-export class LiveComponent {
+export class LiveComponent implements OnInit {
+  constructor(private _api: ApiCallService) {}
 
+  ngOnInit(): void {
+    this.loadLiveMatches();
+  }
+
+  private loadLiveMatches() {
+    this._api.getLiveMatches().subscribe((_data) => {
+      next: (data: any) => {
+        console.log(data);
+      };
+      error: (error: Error) => {
+        console.log(error);
+      };
+    });
+  }
 }
