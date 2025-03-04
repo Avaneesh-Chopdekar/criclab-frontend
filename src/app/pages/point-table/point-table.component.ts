bcb32e2fde7298bceb8c71d43b7fd8793d4e955c
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-04 22:39:55 +0530

    feat: generated point-table page component

---
+++ src/app/pages/point-table/point-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-point-table',
+  imports: [],
+  templateUrl: './point-table.component.html',
+  styleUrl: './point-table.component.css'
+})
+export class PointTableComponent {
+
+}
