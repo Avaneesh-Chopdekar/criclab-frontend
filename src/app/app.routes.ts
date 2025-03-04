380fd888f944869cd8d7d7ab7580ceba41ede63f
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-04 22:55:45 +0530

    feat: added routing with common navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LiveComponent } from './pages/live/live.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'live',
+    component: LiveComponent,
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+  },
+  {
+    path: 'point-table',
+    component: PointTableComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
