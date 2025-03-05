95d84a1a726606c9613214118787f1ced675b3e3
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-05 11:19:55 +0530

    feat: implemented match card component inside live page component

--- src/app/pages/live/live.component.ts
+++ src/app/pages/live/live.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiCallService } from '../../services/api-call.service';
-import { error } from 'console';
+import { MatchSummary } from '../../models/match-summary.model';
+import { MatchCardComponent } from '../../components/match-card/match-card.component';
 
 @Component({
   selector: 'app-live',
-  imports: [],
+  imports: [MatchCardComponent],
   templateUrl: './live.component.html',
   styleUrl: './live.component.css',
 })
 export class LiveComponent implements OnInit {
   constructor(private _api: ApiCallService) {}
 
+  matches: MatchSummary[] = [];
+
   ngOnInit(): void {
     this.loadLiveMatches();
   }
 
   private loadLiveMatches() {
-    this._api.getLiveMatches().subscribe((_data) => {
+    this._api.getLiveMatches().subscribe({
       next: (data: any) => {
-        console.log(data);
-      };
-      error: (error: Error) => {
+        // console.log(data);
+        this.matches = data;
+      },
+      error: (error) => {
         console.log(error);
-      };
+      },
     });
   }
 }
