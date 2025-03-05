433ec1a618ca8822e17511f50311692ab63060a0
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-05 11:27:43 +0530

    feat: implemented match card component in history page component

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../../services/api-call.service';
+import { MatchSummary } from '../../models/match-summary.model';
+import { MatchCardComponent } from '../../components/match-card/match-card.component';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [MatchCardComponent],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.css'
+  styleUrl: './history.component.css',
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  matches: MatchSummary[] = [];
 
+  constructor(private _api: ApiCallService) {}
+
+  ngOnInit() {
+    this._api.getAllMatches().subscribe({
+      next: (data: any) => {
+        this.matches = data as MatchSummary[];
+      },
+      error: (err) => {
+        console.log('Error fetching data', err);
+      },
+    });
+  }
 }
