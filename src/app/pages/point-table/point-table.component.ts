2c0129547ddd0550c686ea8c0e6d5ed62557def2
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-05 18:56:10 +0530

    feat: displayed data from api in point-table component

--- src/app/pages/point-table/point-table.component.ts
+++ src/app/pages/point-table/point-table.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../../services/api-call.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-point-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './point-table.component.html',
-  styleUrl: './point-table.component.css'
+  styleUrl: './point-table.component.css',
 })
-export class PointTableComponent {
+export class PointTableComponent implements OnInit {
+  groupBTable: Array<Array<string>> = [];
+  groupATable: Array<Array<string>> = [];
 
+  constructor(private _api: ApiCallService) {}
+
+  ngOnInit(): void {
+    this._api.getPointTable().subscribe({
+      next: (data: any) => {
+        // console.log(data);
+        this.groupBTable = data.slice(0, 5);
+        this.groupATable = data.slice(5, 10);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
