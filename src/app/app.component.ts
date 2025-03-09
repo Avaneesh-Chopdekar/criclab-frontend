29a48cc562eebf2629c141a60280484b06834c99
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-09 11:36:20 +0530

    feat: registered AuthInterceptor in app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, NavbarComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
