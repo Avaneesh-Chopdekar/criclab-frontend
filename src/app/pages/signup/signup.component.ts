b1233e7cab943928dbbdcd564bedebf15802ba2f
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-09 12:37:05 +0530

    feat: added functional signup for admin users

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,23 +1,91 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-  email = '';
-  password = '';
+  signupForm: FormGroup;
+  invalidPasswordMessage =
+    'Password must be at least 8 characters long, 30 characters maximum and contain at least one lowercase letter, one uppercase letter, one digit and one special character';
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.signupForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\S+$).{8,30}$/
+            ),
+          ],
+        ],
+        confirmPassword: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\S+$).{8,30}$/
+            ),
+          ],
+        ],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  // Access form controls for error handling
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword');
+  }
+
+  // Submit handler
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log('signup Data:', this.signupForm.value);
+      this.signup();
+    } else {
+      this.signupForm.markAllAsTouched(); // Highlight errors
+    }
+  }
 
   signup() {
-    this.auth.login({ email: this.email, password: this.password }).subscribe({
-      next: () => this.router.navigate(['/admin-panel']),
-      error: (err) => console.log('Signup failed', err),
-    });
+    this.auth
+      .signup({ email: this.email?.value, password: this.password?.value })
+      .subscribe({
+        next: () => this.router.navigate(['/admin-panel']),
+        error: (err) => console.log('Signup failed', err),
+      });
   }
 }
